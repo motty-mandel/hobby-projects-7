167f28f5fa04329159715007d744e3c7f129f5db
motty mandel <mandelmottyisrael65@gmail.com>
2023-09-10 12:21:54 -0400

    initial commit

---
+++ 02-Challenge/Develop/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3001
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening at port ${PORT}`)
+});
