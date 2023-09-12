3465557eeed397727d6ad3c71ffc660011c61a7a
motty mandel <mandelmottyisrael65@gmail.com>
2023-09-11 23:12:36 -0400

    added a lot of the code for the note taker

--- 02-Challenge/Develop/routes/notes.js
+++ 02-Challenge/Develop/routes/notes.js
@@ -1,2 +1,31 @@
-const noteRoute = require('express').Router();
-const { readAndAppend } = require('../helpers/fsUtils');
+const nR = require('express').Router();
+const { readAndAppend, readFromFile } = require('../helper/fsUtils');
+const fs = require('fs');
+
+nR.get('/', (req, res) =>
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+nR.post('/', (req, res) => {
+    // // Destructuring assignment for the items in req.body
+    // const { title, text } = req.body;
+
+    // // If all the required properties are present
+    // if (req.body) {
+    //     // Variable for the object we will save
+    //     const newNote = {
+    //         title,
+    //         text,
+    //     };
+
+    //     readAndAppend(newNote, './db/db.json');
+
+    //     res.json(response);
+    // } else {
+    //     res.json('Error in posting note');
+    // }
+    const {title, text} = req.body;
+    console.log(req.body);
+});
+
+module.exports = nR;
--- 02-Challenge/Develop/server.js
+++ 02-Challenge/Develop/server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const path = require('path');
+const notesRoute = require('./routes/notes')
+
 const PORT = 3001;
 const app = express();
 
 app.use(express.static('public'));
+app.use('/notes', notesRoute);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+
 
 app.listen(PORT, () => {
     console.log(`App listening at port ${PORT}`);
